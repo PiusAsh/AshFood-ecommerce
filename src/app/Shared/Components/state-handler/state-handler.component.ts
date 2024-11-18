91017039935cb5575cc38e729a62d4a245342a4d
PiusAsh <pius1ash@gmail.com>
2024-11-18 17:00:50 +0100

    Added state handler

--- src/app/Shared/Components/state-handler/state-handler.component.ts
+++ src/app/Shared/Components/state-handler/state-handler.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-state-handler',
   templateUrl: './state-handler.component.html',
-  styleUrls: ['./state-handler.component.css']
+  styleUrls: ['./state-handler.component.css'],
 })
 export class StateHandlerComponent {
+  @Input() isLoading: boolean = false;
+  @Input() hasError: boolean = false;
+  @Input() isEmpty: boolean = false;
+  @Input() data: any;
 
+  @Input() name: string = 'data';
+
+  @Input() loadingMessage: string = 'Loading...';
+  @Input() errorMessage: string;
+  @Input() emptyMessage: string;
+
+  @Input() loadingImage: string;
+  @Input() errorImage: string;
+  @Input() emptyImage: string;
+
+  @Output() retry = new EventEmitter<void>();
 }
